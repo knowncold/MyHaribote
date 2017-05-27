8cc6c121d7de3e910e6d6275a2594ddf0c1a3bb9
Zed Xu <knowncold@yahoo.com>
2017-05-28 00:34:08 +0800

    修改Day1的文件结构
    补充Day1的第三个img

---
+++ Day 1/img/run.bat
@@ -0,0 +1,2 @@
+copy os.img ..\..\tolset\z_tools\qemu\fdimage0.bin
+..\..\tolset\z_tools\make.exe -C ..\..\tolset\z_tools\qemu
--- Day 1/run.bat
+++
@@ -1,2 +0,0 @@
-copy os.img ..\tolset\z_tools\qemu\fdimage0.bin
-..\tolset\z_tools\make.exe -C ..\tolset\z_tools\qemu
